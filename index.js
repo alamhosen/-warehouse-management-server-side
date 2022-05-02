b50d7ae961349af492162edfe9f49a30d93aa1c7
Alam Hosen <alamhosen.web@gmail.com>
2022-05-02 15:27:41 +0600

    update and delete item api added

--- index.js
+++ index.js
@@ -40,6 +40,30 @@ async function run() {
             res.send(result);
         })
 
+
+        // update
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        // Delete item
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally { }
 }
