7a97244f7f950e4c5c315dc3fd5d302a4ae2ab6f
Alam Hosen <alamhosen.web@gmail.com>
2022-05-02 15:04:44 +0600

    single product api added

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
             res.send(products);
         })
 
+        // get single product
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await productCollection.findOne(query);
+            res.send(service);
+        })
+
     }
     finally { }
 }
