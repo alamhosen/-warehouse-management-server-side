f0e2f97877b4b4ff710f353bff112686aada9945
Alam Hosen <alamhosen.web@gmail.com>
2022-05-02 15:07:32 +0600

    add new product api created

--- test.js
+++ test.js
@@ -1,18 +1,4 @@
- 
-// get single product
-app.get('/product/:id', async (req, res) =>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)};
-    const service = await productCollection.findOne(query);
-    res.send(service);
-})
 
-// add new product
-app.post('/product', async(req, res) =>{
-    const newProduct = req.body;
-    const result = await productCollection.insertOne(newProduct);
-    res.send(result);
-})
 
 // update
 app.put('/product/:id', async(req, res) =>{
